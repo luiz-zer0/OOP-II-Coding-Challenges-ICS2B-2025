3ea6ca74495827e6927321dedd52e11a5c235bcb
luiz-zer0 <louisngatia47@gmail.com>
2025-04-29 13:59:19 +0300

    challenge 6:Import Random

---
+++ challenges/random.java
@@ -0,0 +1,45 @@
+import java.io.Console;
+import java.util.Random;
+
+public class random {
+    public static void main(String[] args) {
+        Random random = new Random();
+int daysUntilExpiration = random.nextInt(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration== 1) 
+{
+discountPercentage=20;
+}
+if (daysUntilExpiration<= 5) 
+{
+discountPercentage=10;
+}
+if (daysUntilExpiration<1) 
+{
+System.out.println("Your subscription has expired!");
+}
+
+else if (daysUntilExpiration == 1) 
+{
+System.out.println("Your subscription expires within a day!");
+System.out.println("Renew now and save " + discountPercentage + " %!");
+}
+else if (daysUntilExpiration<= 5) 
+{
+System.out.println("Your subscription expires in: " + daysUntilExpiration + " days.");
+System.out.println("Renew now and save" + " "+discountPercentage + " %!");
+
+}
+else if (daysUntilExpiration<= 10) {
+System.out.println("Your subscription expires in:" + daysUntilExpiration + " days.");
+
+System.out.println("Your subscription will expire soon. Renew now!");}
+
+
+
+    }
+         
+
+    
+}
